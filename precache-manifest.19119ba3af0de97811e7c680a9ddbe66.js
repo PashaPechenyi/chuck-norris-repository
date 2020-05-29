50f3d7c44f873a361def937041b78768689eab6a
PashaPechenyi <pashapechenn@gmail.com>
2020-05-29 14:24:00 +0300

    Updates

---
+++ precache-manifest.19119ba3af0de97811e7c680a9ddbe66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4a42e1582e4c14ea88fd860183b8ea",
+    "url": "/chuck-norris-repository/index.html"
+  },
+  {
+    "revision": "3f5cd533d0f81d2115bc",
+    "url": "/chuck-norris-repository/static/css/main.5e692fdf.chunk.css"
+  },
+  {
+    "revision": "7513fe1819c172b7d133",
+    "url": "/chuck-norris-repository/static/js/2.221b8bc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chuck-norris-repository/static/js/2.221b8bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5cd533d0f81d2115bc",
+    "url": "/chuck-norris-repository/static/js/main.7e58ed1e.chunk.js"
+  },
+  {
+    "revision": "88c323ef4eb5c3a9180a",
+    "url": "/chuck-norris-repository/static/js/runtime-main.da3bbe9c.js"
+  },
+  {
+    "revision": "09d6d5da9b42346c72fcbcb550413781",
+    "url": "/chuck-norris-repository/static/media/closeIcon.09d6d5da.png"
+  },
+  {
+    "revision": "8f33806e51d669ed04795c7612cd892d",
+    "url": "/chuck-norris-repository/static/media/commentIcon.8f33806e.png"
+  },
+  {
+    "revision": "d353f6c897a8e1dc1f62127667af3030",
+    "url": "/chuck-norris-repository/static/media/menuIcon.d353f6c8.png"
+  }
+]);
